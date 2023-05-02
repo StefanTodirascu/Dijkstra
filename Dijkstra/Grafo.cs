ca1c5de7488e01ccd2b3fd1527b2c800a24e2df7
Stefan <razvanstefan.todirascu@iisviolamarchesini.edu.it>
2023-05-02 21:01:50 +0200

    primo commit

---
+++ Dijkstra/Grafo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    internal class Grafo
+    {
+        List<Nodo> nodi;
+        public Grafo(Nodo n)
+        {
+            nodi = new List<Nodo>();
+            nodi.Add(n);
+            for (int i = 0; i < nodi.Count; i++)
+            {
+                foreach (Arco a in nodi[i].Archi)
+                {
+                    if (nodi.Find(x => x.Nome.Equals(a.Destinazione.Nome)) is null) //controllo che non ci siano nodi omonimi 
+                        nodi.Add(a.Destinazione);
+                }
+                    
+            }
+                
+        }
+        public List<Riga> CamminoMinimo()
+        {
+            List<Riga> finale = new List<Riga>();
+            List<Riga> indagine = new List<Riga>();
+            foreach (Nodo nodo in nodi)
+                indagine.Add(new Riga(nodo.Nome, "", int.MaxValue));
+            indagine[0].Costo = 0;
+            return new List<Riga>();
+        }
+    }
+}
---
+++ Dijkstra/Nodo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    internal class Nodo
+    {
+        static int index;
+        string nome;
+        List<Arco> archi;
+        List<Riga> camminominimo;
+        public Nodo()
+        {
+            archi = new List<Arco>();
+            index++;
+            nome = index.ToString();
+        }
+        public Nodo(string nome) : this()
+        {
+            this.nome += nome;
+        }
+
+        public string Nome { get => nome; }
+        internal List<Arco> Archi { get => archi; }
+
+        internal List<Riga> CamminoMinimo()
+        {
+            Grafo g = new Grafo(this);
+
+            return camminominimo;
+        }
+    }
+}
